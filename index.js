4f9a9d77ca3ebff66ae853c45e8abe6b7f1df1ce
Yevhen-Kryvdiuk <krivachacha@gmail.com>
2019-05-29 14:19:05 +0300

    solution

---
+++ index.js
@@ -0,0 +1,13 @@
+function isJSON(str) {
+    try {
+        JSON.parse(str);
+        console.log(true);
+    } catch (error) {
+        console.log(false);
+    }
+}
+
+isJSON('{"name": "Michael", "age": 45}'); // true
+isJSON('{name: "Michael", age: 45}'); // false
+isJSON('false'); // true
+isJSON('abc'); // false

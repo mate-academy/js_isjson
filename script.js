14d292d8c53d1dfbbdec2bb18cf9574ef7d688a1
VladimirTi <tytmes@mail.ru>
2019-05-29 15:55:15 +0300

    Vladimir's Solution

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function isJSON (jsonStr) {
   try {
-    const obj = JSON.parse(jsonStr)
-    console.log('true')
+    const obj = JSON.parse(jsonStr);
+    console.log('true');
   } catch (error) {
-    console.log('false')
+    console.log('false');
   }
 }
-
 
 isJSON('{"name": "Michael", "age": 45}'); // true
 isJSON('{name: "Michael", age: 45}'); // false

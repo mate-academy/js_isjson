368cac905d70f69edbc1209988f9b115639f59b0
Onisko <Onisko@MacBook.local>
2019-05-31 14:25:35 +0300

    solution

---
+++ json.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function isJSON(str) {
+  try {
+    JSON.parse(str);
+    return true;
+  } catch(e) {
+    return false;
+  }
+}
+
+console.log(isJSON('{"name": "Michael", "age": 45}'));
+console.log(isJSON('{"name": "Michael", age: 45}'));
+console.log(isJSON('false'));
+console.log(isJSON('abc'));

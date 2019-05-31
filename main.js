377810f4b7104416539ad31b5a240aca4b9e4aaa
Stas Tselovanskyi <solmirx1@gmail.com>
2019-05-31 17:42:27 +0300

    solution

---
+++ main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function isJSON(str) {
+    try {
+        JSON.parse(str);
+    } catch (error) {
+        return false;
+    }
+    return true;
+}
+
+console.log(isJSON('{"name": "Michael", "age": 45}')); // true
+console.log(isJSON('{name: "Michael", age: 45}')); // false
+console.log(isJSON('false')); // true
+console.log(isJSON('abc')); // false

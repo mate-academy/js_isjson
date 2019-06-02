28b2de7a96d3bb148fd78065524cd51571dd24fc
Vitalii Pshychenko <pshychenko94@gmail.com>
2019-06-02 19:55:34 +0300

    solution

---
+++ main.js
@@ -0,0 +1,13 @@
+function isJSON(string) {
+  try {
+    JSON.parse(string);
+    return true;
+  } catch(error) {
+    return false;
+  }
+}
+
+isJSON('{"name": "Michael", "age": 45}');
+isJSON('{name: "Michael", age: 45}');
+isJSON('false');
+isJSON('abc');

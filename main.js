75a5b5cfb8910732bf6b58fe3b21a2cafe9af1da
Fandorin <SherJlok15@gmail.com>
2019-06-03 18:29:04 +0300

    Solution

---
+++ main.js
@@ -0,0 +1,14 @@
+function isJSON(str) {
+  try {
+    JSON.parse(str);
+    return true;
+  }
+  catch (err) {
+    return false;
+  }
+}
+
+console.log(isJSON('{"name": "Michael", "age": 45}'));
+console.log(isJSON('{name: "Michael", age: 45}'));
+console.log(isJSON('false'));
+console.log(isJSON('abc'));

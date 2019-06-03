fcaeac8262ee991fb59a6e969ace5aaa67c4f749
annakondr <tyukondra@gmail.com>
2019-06-03 17:54:56 +0300

    fixed_spaces

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 'use strict';
 function isJSON(string) {
-    try {
-        JSON.parse(string);
-    } catch (error) {
-        return false;
-    }
-    return true;
+  try {
+    JSON.parse(string);
+  } catch (error) {
+    return false;
+  }
+  return true;
 }
 
 isJSON('{"name": "Michael", "age": 45}');

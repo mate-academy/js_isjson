facf94d93efcee88e8e1ac9647713f377ed7729c
Karpyshyn Roman <karpyshyn.roman@gmail.com>
2019-10-22 13:23:40 +0300

    s

---
+++ main.js
@@ -0,0 +1,14 @@
+
+function isJSON(data) {
+  try {
+    JSON.parse(data);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+console.log(isJSON('{"name": "Michael", "age": 45}'));
+console.log(isJSON('{name: "Michael", age: 45}'));
+console.log(isJSON('false'));
+console.log(isJSON('abc'));
